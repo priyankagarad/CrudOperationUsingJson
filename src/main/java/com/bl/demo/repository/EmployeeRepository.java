360a9a3e1a5cdc33f2b2979361b40209e6e0459d
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-24 00:36:24 +0530

    ADDED: Created Method for Load Data

--- src/main/java/com/bl/demo/repository/EmployeeRepository.java
+++ src/main/java/com/bl/demo/repository/EmployeeRepository.java
@@ -1,6 +1,31 @@
 package com.bl.demo.repository;
+import com.bl.demo.model.Employee;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.*;
+import java.util.List;
 
 public class EmployeeRepository {
     public List<Employee> employeeList;
-    
+
+    public List<Employee> loadEmployeeData() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            InputStream  inputStream = new FileInputStream(new File("./src/main/resources/Empoyee.json"));
+            com.fasterxml.jackson.core.type.TypeReference<List<Employee>> typeReference = new TypeReference<List<Employee>>() {
+            };
+            employeeList = mapper.readValue(inputStream, typeReference);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return employeeList;
+    }
 }
