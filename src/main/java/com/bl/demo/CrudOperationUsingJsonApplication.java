90fd9e2c6d43324d32403e23465690756a1448cd
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-23 16:07:14 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/bl/demo/CrudOperationUsingJsonApplication.java
@@ -0,0 +1,12 @@
+package com.bl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationUsingJsonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrudOperationUsingJsonApplication.class, args);
+    }
+
+}
